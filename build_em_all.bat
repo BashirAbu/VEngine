76ce94285484eb36ad196c5e95bc47963d430b9e
BashirAbu <bashir.t.abugharsa@gmail.com>
2024-11-12 23:14:23 +0200

    updated build em all

--- build_em_all.bat
+++ build_em_all.bat
@@ -1,8 +1,16 @@
+@echo off
 set build_type=%1
 cd VEngine\third_party\raylib
 mkdir build
 cd build
 cmake -DBUILD_SHARED_LIBS=ON -DBUILD_EXAMPLES=OFF -DBUILD_GAMES=OFF -DBUILD_TESTING=OFF ..
+cmake --build . --config %build_type%
+cd ..
+cd ..
+cd glfw
+mkdir build
+cd build
+cmake ..
 cmake --build . --config %build_type%
 cd ..
 cd ..
